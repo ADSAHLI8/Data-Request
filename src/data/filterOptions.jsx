7ea2b0e4dedf10dd4a7ea930e1506b8a3d1b5aae
Adnane Sahli <adnane8sahli@gmail.com>
2024-10-11 10:48:01 +0100

    front end file

--- src/data/filterOptions.jsx
+++
@@ -1,9 +0,0 @@
-const filterOptions = [
-    { value: 'Enabled', name: 'Enabled' },
-    { value: 'Disabled', name: 'Disabled' },
-    { value: 'Enabled (DUPS)', name: 'Enabled (DUPS)' },
-
-  ]
-
-
-export default filterOptions ;
